b759f6524a8564fd2772f8e3c77627c8eafbd2a6
David Posada <#davidposada22@gmail.com>
2019-03-08 12:03:03 -0500

    configuracion inicial del proyecto

---
+++ operacionesForGit/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operacionesForGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejemplo Operaciones Aritmeticas para Git");
+
+            Console.WriteLine("Numero Uno:");
+            short numUno = short.Parse(Console.ReadLine());
+
+            Console.WriteLine("Numero Dos:");
+            short numDos = short.Parse(Console.ReadLine());
+
+
+            //
+            Console.ReadLine();
+        }
+    }
+}
